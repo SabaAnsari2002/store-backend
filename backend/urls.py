a18e70d6c7513003cc875f0c4cce861566a44a4d
Saba <saba.ansari4647@gmail.com>
2025-05-26 02:17:54 +0330

    Building a new structure

--- backend/urls.py
+++ backend/urls.py
@@ -1,22 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import RegisterView, LoginView
-from sellerpanel.views import dashboard, product_list, edit_product, delete_product, order_list, delete_order, update_order_status , order_detail
-
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path('api/users/', include('users.urls')),
+    path('api/sellers/', include('sellers.urls')),
+    path('api/products/', include('products.urls')),
 
-    path('products/', product_list, name='product_list'),
-    path('products/edit/<int:pk>/', edit_product, name='edit_product'),
-    path('products/delete/<int:pk>/', delete_product, name='delete_product'),
-
-    path('orders/', order_list, name='order_list'),
-    path('orders/delete/<int:pk>/', delete_order, name='delete_order'),
-    path('orders/update/<int:pk>/', update_order_status, name='update_order_status'),
-    path('orders/detail/<int:pk>/', order_detail, name='order_detail'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
---
+++ users/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from .models import CustomUser
+from .serializers import UserSerializer
+
+class RegisterUser(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
