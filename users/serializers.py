dc0470188cf1ba1f12ffd20ca8745cec6b891936
Saba <saba.ansari4647@gmail.com>
2025-06-04 01:22:46 +0330

    [feat]: add API to get and put user information

--- users/serializers.py
+++ users/serializers.py
@@ -2,12 +2,31 @@ from rest_framework import serializers
 from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False, allow_blank=True)
+    new_password = serializers.CharField(write_only=True, required=False, allow_blank=True)
+
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'phone', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'email', 'phone', 'password', 'new_password')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'new_password': {'write_only': True}
+        }
 
-    
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        new_password = validated_data.pop('new_password', None)
+
+        # Verify current password if changing password
+        if new_password:
+            if not password:
+                raise serializers.ValidationError({"password": "Current password is required to set a new password."})
+            if not instance.check_password(password):
+                raise serializers.ValidationError({"password": "Current password is incorrect."})
+            instance.set_password(new_password)
+
+        return super().update(instance, validated_data)
+
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
--- users/views.py
+++ users/views.py
@@ -4,6 +4,27 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import CustomUser
 from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 class RegisterUser(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
